ef8c0039a2f3e492794ea64ec082c3eb6f1c5a29
Jorge Núñez <94089914+KuliesJ@users.noreply.github.com>
2022-06-19 13:53:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,29 @@
 #pip install requests, flask, flask_restful
 #
 
+#DATABASE USER
+
+#END OF DATABASE USER
+
 from flask import Flask
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-class Usuario_info(Resource):
+class Producto_info(Resource):
     def get(self):
-        return {
-                "Nombre": "Don Pepito",
-                "Correo": "DonPepito@DonJose.com",
-                "Password": "incorrecta",
-                "Carrito": {
-                        "Producto1": "Cubo_ejemplo",
-                        "Producto2": "Cubo_ejemplo2"
-                        }
-                }
+        return None
+
+class Usuario_info(Resource):
+    def get(self, name, test):
+        return {"data": name, "test": test}
     
-api.add_resource(Usuario_info, "/usuario_info")
+    def post(self):
+        return {"data": "Posted"}
+    
+#api.add_resource(Usuario_info, "/usuario_info")
+api.add_resource(Usuario_info, "/usuario_info/<string:name>/<int:test>")
 
 if __name__ == '__main__':
     app.run(debug=True)
