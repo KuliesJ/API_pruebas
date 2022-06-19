f7f4d3d15b2b629932c17b1a5db7a79cb2c32e7e
Jorge Núñez <94089914+KuliesJ@users.noreply.github.com>
2022-06-19 13:53:23 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,6 +2,16 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "usuario_info")
+entrada_datos_nombre = str(input("Nombre de usuario: "))
+entrada_datos_edad = str(input("Edad de usuario: "))
+
+res = BASE + "usuario_info/" + entrada_datos_nombre + "/" +entrada_datos_edad
+print(res)
+
+response = requests.get(BASE + "usuario_info/Kulies/18")
+
+print(response.json())
+
+response = requests.get(res)
 
 print(response.json())
